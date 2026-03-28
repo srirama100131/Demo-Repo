a23d88f2b328b5ee7485ac7538c5b1052619ee01
rajubandi <rajubandi@gmail.com>
2026-03-28 11:38:13 +0530

    2 added data in all sources

--- vnt.tf
+++ vnt.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "bandi" {
     name = "vnet-bandi"
-    resource_group_name = azurerm_resource_group.bandi.name
-    location            = azurerm_resource_group.bandi.location 
+    resource_group_name = azurerm_resource_group.malli.name
+    location            = azurerm_resource_group.malli.location 
     address_space       = ["10.0.0.0/16"]
 }
